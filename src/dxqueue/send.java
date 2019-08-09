67e462edd417f86864097027ee2c5db4111bb203
Lothar Haeger <lothar.haeger@is4it.de>
2019-08-09 11:15:36 +0200

    Added tracing, exception handling, javadocs

--- src/dxqueue/send.java
+++ src/dxqueue/send.java
@@ -1,9 +1,10 @@
 package dxqueue;
 
 import com.novell.nds.dhutil.JCHelper;
-import com.novell.nds.dirxml.driver.XmlDocument;
+import com.novell.nds.dirxml.driver.Trace;
 import com.novell.nds.dirxml.engine.MiscDS;
 import com.novell.nds.dirxml.engine.NdsDtd;
+import com.novell.nds.dirxml.engine.OperationData;
 import com.novell.nds.dirxml.engine.ThreadGroupVars;
 import com.novell.nds.dirxml.engine.XdsUtil;
 import com.novell.nds.dirxml.util.DxWire;
@@ -12,87 +13,105 @@ import java.io.ByteArrayOutputStream;
 import novell.jclient.JCContext;
 import novell.jclient.JCException;
 import novell.jclient.JClient;
-//import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public abstract class send
-{
+public abstract class send {
+	
+	// An updated version of https://community.microfocus.com/t5/Identity-Manager-Tips/Sending-a-XDS-Message-from-one-driver-to-another/ta-p/1777175
 
-  public static Element sendQueueEvent(String driverDN, Element element)
-  {
-    ByteArrayOutputStream baos;
-    JCContext context = null;
-    try
-    {
-      context = new JCContext(0, MiscDS.getTreeName(), "00.\\+=*'");
+	public static final String VERSION = "1.1.0";
 
-      JCHelper.loginAsServer(context);
-      context.authenticate();
+	public static Element sendQueueEvent(String driverDN, Element element) {
 
-      String currentDriverDN = (String)ThreadGroupVars.get("vrDriverDN");
-      if (currentDriverDN == null)
-      {
-        throw new JCException("getEffectivePriviledges()", -672);
-      }
-      context.nameToID(1, driverDN);
-      if ((context.getEffectivePrivileges(currentDriverDN, "[Entry Rights]") & 0x10) == 0L)
-      {
-        throw new JCException("getEffectivePriviledges()", -672);
-      }
+		ByteArrayOutputStream baos;
+		JCContext context = null;
+		OperationData opdata = new OperationData(null);
+		Trace tracer = new Trace("dxqueue");
 
-      if (!(element.getNodeName().equals("nds")))
-      {
-        Element rootElement = NdsDtd.createDoc("input");
-        XdsUtil.graftSubtree(rootElement, element);
-        element = rootElement.getOwnerDocument().getDocumentElement();
-      }
+		try {
 
-      baos = new ByteArrayOutputStream();
-      DOMWriter domWriter = new DOMWriter(element, baos, "UTF-8");
-      domWriter.setEncoding("UTF-8");
-      domWriter.write();
-      byte[] bytes = baos.toByteArray();
-      DxWire wireCtor = getDxWire();
-      byte[] result = sendWireRequest(context, wireCtor.constructQueueEvent(driverDN, bytes));
-      Element output = new XmlDocument("<success/>").getDocument().getDocumentElement();
-      return output;
-    }
-    catch (Throwable t)
-    {
-      return NdsDtd.createStatusDocument(t, null).getDocumentElement();
-    }
-    finally
-    {
-      if (context != null)
-      {
-        context.free();
-      }
-    }
-  }
+			String treeName = MiscDS.getTreeName();
+			context = new JCContext(0, treeName, "00.\\+=*'");
 
-  private static DxWire getDxWire()
-  {
-    DxWire wireCtor = new DxWire();
-    wireCtor.setDnFormat(0);
-    return wireCtor;
-  }
+			tracer.trace("Authenticating...", 4);
+			JCHelper.loginAsServer(context);
+			context.authenticate();
 
-  private static byte[] sendWireRequest(JCContext context, DxWire.DxWireData wireData)
-    throws JCException
-  {
-    byte[] replyBuffer = new byte[wireData.getResponseSize()];
-    int replySize = JClient.ndsRequest(context, wireData.getVerb(), wireData.getRequestData().length, wireData.getRequestData(), replyBuffer.length, replyBuffer);
+			String currentDriverDN = (String) ThreadGroupVars.get("vrDriverDN");
+			tracer.trace("Verifying access to " + currentDriverDN + "...", 4);
+			if (currentDriverDN == null) {
+				throw new JCException("getEffectivePriviledges()", -672);
+			}
 
-    if (replySize == 0)
-    {
-      return null;
-    }
-    if (replySize != replyBuffer.length)
-    {
-      byte[] temp = new byte[replySize];
-      System.arraycopy(replyBuffer, 0, temp, 0, replySize);
-      replyBuffer = temp;
-    }
-    return replyBuffer;
-  }
+			if (!(driverDN.startsWith("\\"))) {
+				tracer.trace("Converting " + driverDN + " to absolute path...", 4);
+				driverDN = "\\" + treeName + "\\" + driverDN;
+			}
+
+			tracer.trace("Verifying access to " + driverDN + "...", 4);
+			context.nameToID(1, driverDN);
+			if ((context.getEffectivePrivileges(currentDriverDN, "[Entry Rights]") & 0x10) == 0L) {
+				throw new JCException("getEffectivePriviledges()", -672);
+			}
+
+			String rootNodeName = element.getNodeName();
+			if (!(rootNodeName.equals("nds"))) {
+				if (rootNodeName.matches("add|modify|rename|move|delete|trigger|sync")) {
+					tracer.trace("Wrapping XDS document around " + rootNodeName + " node...", 4);
+					Element rootElement = NdsDtd.createDoc("input");
+					XdsUtil.graftSubtree(rootElement, element);
+					element = rootElement.getOwnerDocument().getDocumentElement();
+				} else {
+					return NdsDtd.createStatusDocument(NdsDtd.SL_ERROR, opdata, "Invalid document: " + rootNodeName + " is not supported.").getDocumentElement();
+				}
+			}
+
+			tracer.trace("Converting document to byte array...", 4);
+			baos = new ByteArrayOutputStream();
+			DOMWriter domWriter = new DOMWriter(element, baos, "UTF-8");
+			domWriter.setEncoding("UTF-8");
+			domWriter.write();
+			byte[] bytes = baos.toByteArray();
+
+			tracer.trace("Submitting document...", 4);
+			DxWire wireCtor = getDxWire();
+			@SuppressWarnings("unused")
+			byte[] result = sendWireRequest(context, wireCtor.constructQueueEvent(driverDN, bytes));
+
+			return NdsDtd.createStatusDocument(NdsDtd.SL_SUCCESS, opdata, "Submitted document to " + driverDN).getDocumentElement();
+
+		} catch (Throwable t) {
+
+			return NdsDtd.createStatusDocument(t, opdata).getDocumentElement();
+
+		} finally {
+
+			if (context != null) {
+				context.free();
+			}
+		}
+	}
+
+	private static DxWire getDxWire() {
+
+		DxWire wireCtor = new DxWire();
+		wireCtor.setDnFormat(0);
+		return wireCtor;
+	}
+
+	private static byte[] sendWireRequest(JCContext context, DxWire.DxWireData wireData) throws JCException {
+
+		byte[] replyBuffer = new byte[wireData.getResponseSize()];
+		int replySize = JClient.ndsRequest(context, wireData.getVerb(), wireData.getRequestData().length, wireData.getRequestData(),
+				replyBuffer.length, replyBuffer);
+
+		if (replySize == 0) {
+			return null;
+		} else if (replySize != replyBuffer.length) {
+			byte[] temp = new byte[replySize];
+			System.arraycopy(replyBuffer, 0, temp, 0, replySize);
+			replyBuffer = temp;
+		}
+		return replyBuffer;
+	}
 }
